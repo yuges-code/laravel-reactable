69b3b85d6fe44bd5ea90a072b469787d76dc9d6e
Georgy <goshasafonov@yandex.ru>
2025-02-18 16:15:27 +0300

    added trait

--- src/Enums/ReactionType.php
+++ src/Enums/ReactionType.php
@@ -2,22 +2,13 @@
 
 namespace Yuges\Reactable\Enums;
 
-use BackedEnum;
-use Yuges\Reactable\Config\Config;
+use Yuges\Reactable\Traits\HasReactionTypeInterface;
 use Yuges\Reactable\Interfaces\ReactionType as ReactionTypeInterface;
 
 enum ReactionType: int implements ReactionTypeInterface
 {
+    use HasReactionTypeInterface;
+
     case Like = 1;
     case Dislike = 2;
-
-    public function icon(): BackedEnum
-    {
-        return Config::getReactionIconEnumClass()::{$this->name};
-    }
-
-    public function weight(): BackedEnum
-    {
-        return Config::getReactionWeightEnumClass()::{$this->name};
-    }
 }
